64301e3a0e4fb0b0f9dc054efdffd779c2db0660
Tempestar11 <cxx504@york.ac.uk>
2025-06-10 14:54:27 +0800

    Added PI page

---
+++ pages/6_🌍_Planetary_Interaction.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Planetary Interaction Guide",
+    page_icon="🌍",
+)
+
+st.title("🌍 Planetary Interaction Guide")
+st.markdown("""It's a love-hate relationship honestly.""")
