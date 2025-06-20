a5b86d65f8d1a97d7769a0ca58a7c219f18061c6
Tempestar11 <cxx504@york.ac.uk>
2025-06-20 20:50:43 +0800

    Different naming scheme for images for PVE

--- pages/1_💥_PvE.py
+++ pages/1_💥_PvE.py
@@ -136,83 +136,83 @@ class_option = st.pills(
 if class_option == "C2":
     st.caption("Combat sites")
     with st.expander("Perimeter Checkpoint"):
-        st.image('combat_sites/C2/perimeter_checkpoint.png', caption="Perimeter Checkpoint")
+        st.image('combat_sites/C2/1_perimeter_checkpoint.png', caption="Perimeter Checkpoint")
     with st.expander("Perimeter Hangar"):
-        st.image('combat_sites/C2/perimeter_hangar.png', caption="Perimeter Hangar")
+        st.image('combat_sites/C2/2_perimeter_hangar.png', caption="Perimeter Hangar")
     with st.expander("Sleeper Data Signature Sanctuary"):
-        st.image('combat_sites/C2/sleeper_data_signature_sanctuary.png', caption="Sleeper Data Signature Sanctuary")
+        st.image('combat_sites/C2/3_sleeper_data_signature_sanctuary.png', caption="Sleeper Data Signature Sanctuary")
     with st.expander("The Ruins of Enclave Cohort 27"):
-        st.image('combat_sites/C2/the_ruins_of_enclave_cohort_27.png', caption="The Ruins of Enclave Cohort 27")
+        st.image('combat_sites/C2/4_the_ruins_of_enclave_cohort_27.png', caption="The Ruins of Enclave Cohort 27")
     st.caption("Relic sites")
     with st.expander("Forgotten Perimeter Gateway"):
-        st.image('combat_sites/C2/forgotten_perimeter_gateway.png', caption="Forgotten Perimeter Gateway")
+        st.image('combat_sites/C2/5_forgotten_perimeter_gateway.png', caption="Forgotten Perimeter Gateway")
     with st.expander("Forgotten Perimeter Habitation Coils"):
-        st.image('combat_sites/C2/forgotten_perimeter_habitation_coils.png', caption="Forgotten Perimeter Habitation Coils")
+        st.image('combat_sites/C2/6_forgotten_perimeter_habitation_coils.png', caption="Forgotten Perimeter Habitation Coils")
     st.caption("Data sites")
     with st.expander("Unsecured Perimeter Comms Relay"):
-        st.image('combat_sites/C2/unsecured_perimeter_comms_relay.png', caption="Unsecured Perimeter Comms Relay")
+        st.image('combat_sites/C2/7_unsecured_perimeter_comms_relay.png', caption="Unsecured Perimeter Comms Relay")
     with st.expander("Unsecured Perimeter Transponder Farm"):
-        st.image('combat_sites/C2/unsecured_perimeter_transponder_farm.png', caption="Unsecured Perimeter Transponder Farm")
+        st.image('combat_sites/C2/8_unsecured_perimeter_transponder_farm.png', caption="Unsecured Perimeter Transponder Farm")
 
 elif class_option == "C3":
     st.caption("Combat sites")
     with st.expander("Fortification Frontier Stronghold"):
-        st.image('combat_sites/C3/fortification_frontier_stronghold.png', caption="Fortification Frontier Stronghold")
+        st.image('combat_sites/C3/1_fortification_frontier_stronghold.png', caption="Fortification Frontier Stronghold")
     with st.expander("Outpost Frontier Stronghold"):
-        st.image('combat_sites/C3/outpost_frontier_stronghold_2.png', caption="Outpost Frontier Stronghold")
+        st.image('combat_sites/C3/2_outpost_frontier_stronghold_2.png', caption="Outpost Frontier Stronghold")
     with st.expander("Solar Cell"):
-        st.image('combat_sites/C3/solar_cell.png', caption="Solar Cell")
+        st.image('combat_sites/C3/3_solar_cell.png', caption="Solar Cell")
     with st.expander("The Oruze Construct"):
-        st.image('combat_sites/C3/the_oruze_construct.png', caption="The Oruze Construct")
+        st.image('combat_sites/C3/4_the_oruze_construct.png', caption="The Oruze Construct")
     st.caption("Relic sites")
     with st.expander("Forgotten Frontier Quarantine Outpost"):
-        st.image('combat_sites/C3/forgotten_frontier_quarantine_outpost.png', caption="Forgotten Frontier Quarantine Outpost")  
+        st.image('combat_sites/C3/5_forgotten_frontier_quarantine_outpost.png', caption="Forgotten Frontier Quarantine Outpost")  
     with st.expander("Forgotten Frontier Recursive Depot"):
-        st.image('combat_sites/C3/forgotten_frontier_recursive_depot.png', caption="Forgotten Frontier Recursive Depot")
+        st.image('combat_sites/C3/6_forgotten_frontier_recursive_depot.png', caption="Forgotten Frontier Recursive Depot")
     st.caption("Data sites")
     with st.expander("Unsecured Frontier Database"):
-        st.image('combat_sites/C3/unsecured_frontier_database.png', caption="Unsecured Frontier Database")
+        st.image('combat_sites/C3/7_unsecured_frontier_database.png', caption="Unsecured Frontier Database")
     with st.expander("Unsecured Frontier Reciever"):
-        st.image('combat_sites/C3/unsecured_frontier_receiver.png', caption="Unsecured Frontier Receiver")
+        st.image('combat_sites/C3/8_unsecured_frontier_receiver.png', caption="Unsecured Frontier Receiver")
 
 elif class_option == "C4":
     st.caption("Combat sites")
     with st.expander("Frontier Barracks"):
-        st.image('combat_sites/C4/frontier_barracks.png', caption="Frontier Barracks")
+        st.image('combat_sites/C4/1_frontier_barracks.png', caption="Frontier Barracks")
     with st.expander("Frontier Command Post"):
-        st.image('combat_sites/C4/frontier_command_post.png', caption="Frontier Command Post")
+        st.image('combat_sites/C4/2_frontier_command_post.png', caption="Frontier Command Post")
     with st.expander("Integrated Terminus"):
-        st.image('combat_sites/C4/integrated_terminus.png', caption="Integrated Terminus")
+        st.image('combat_sites/C4/3_integrated_terminus.png', caption="Integrated Terminus")
     with st.expander("Sleeper Information Sanctum"):
-        st.image('combat_sites/C4/sleeper_information_sanctum.png', caption="Sleeper Information Sanctum")
+        st.image('combat_sites/C4/4_sleeper_information_sanctum.png', caption="Sleeper Information Sanctum")
     st.caption("Relic sites")
     with st.expander("Forgotten Frontier Conversion Module"):
-        st.image('combat_sites/C4/forgotten_frontier_conversion_module.png', caption="Forgotten Frontier Conversion Module")
+        st.image('combat_sites/C4/5_forgotten_frontier_conversion_module.png', caption="Forgotten Frontier Conversion Module")
     with st.expander("Forgotten Frontier Evacuation Center"):
-        st.image('combat_sites/C4/forgotten_frontier_evacuation_center.png', caption="Forgotten Frontier Evacuation Center")
+        st.image('combat_sites/C4/6_forgotten_frontier_evacuation_center.png', caption="Forgotten Frontier Evacuation Center")
     st.caption("Data sites")
     with st.expander("Unsecured Frontier Digital Nexus"):
-        st.image('combat_sites/C4/unsecured_frontier_digital_nexus.png', caption="Unsecured Frontier Digital Nexus")
+        st.image('combat_sites/C4/7_unsecured_frontier_digital_nexus.png', caption="Unsecured Frontier Digital Nexus")
     with st.expander("Unsecured Frontier Trinary Hub"):
-        st.image('combat_sites/C4/unsecured_frontier_trinary_hub.png', caption="Unsecured Frontier Trinary Hub")
+        st.image('combat_sites/C4/8_unsecured_frontier_trinary_hub.png', caption="Unsecured Frontier Trinary Hub")
 
 elif class_option == "C5":
     st.caption("Combat sites")
     with st.expander("Core Garrison"):
-        st.image("combat_sites/C5/core_garrison.png", caption="Core Garrison")
+        st.image("combat_sites/C5/1_core_garrison.png", caption="Core Garrison")
     with st.expander("Core Stronghold"):
-        st.image("combat_sites/C5/core_stronghold.png", caption="Core Stronghold")
+        st.image("combat_sites/C5/2_core_stronghold.png", caption="Core Stronghold")
     with st.expander("Oruze Osobnyk"):
-        st.image("combat_sites/C5/oruze_osobnyk.png", caption="Oruze Osobnyk")
+        st.image("combat_sites/C5/3_oruze_osobnyk.png", caption="Oruze Osobnyk")
     with st.expander("Quarantine Area"):
-        st.image("combat_sites/C5/quarantine_area.png", caption="Quarantine Area")
+        st.image("combat_sites/C5/4_quarantine_area.png", caption="Quarantine Area")
     st.caption("Relic sites")
     with st.expander("Forgotten Core Data Field"):
-        st.image("combat_sites/C5/forgotten_core_data_field.png", caption="Forgotten Core Data Field")
+        st.image("combat_sites/C5/5_forgotten_core_data_field.png", caption="Forgotten Core Data Field")
     with st.expander("Forgotten Core Information Pen"):
-        st.image("combat_sites/C5/forgotten_core_information_pen.png", caption="Forgotten Core Information Pen")
+        st.image("combat_sites/C5/6_forgotten_core_information_pen.png", caption="Forgotten Core Information Pen")
     st.caption("Data sites")
     with st.expander("Unsecured Frontier Enclave Relay"):
-        st.image("combat_sites/C5/unsecured_frontier_enclave_relay.png", caption="Unsecured Frontier Enclave Relay")
+        st.image("combat_sites/C5/7_unsecured_frontier_enclave_relay.png", caption="Unsecured Frontier Enclave Relay")
     with st.expander("Unsecured Frontier Server Bank"):
-        st.image("combat_sites/C5/unsecured_frontier_server_bank.png", caption="Unsecured Frontier Server Bank")
+        st.image("combat_sites/C5/8_unsecured_frontier_server_bank.png", caption="Unsecured Frontier Server Bank")
